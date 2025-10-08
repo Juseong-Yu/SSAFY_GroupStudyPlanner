5bf5361212d505e6bd6fd33b913840d888e68f8b
RASIT <shjh0815@naver.com>
2025-10-08 17:28:51 +0900

    Edit Signup & User class

--- Backend/accounts/forms.py
+++ Backend/accounts/forms.py
@@ -1,11 +1,20 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
+        fields = ('email', 'username')
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if get_user_model().objects.filter(email=email).exists():
+            raise forms.ValidationError("이미 존재하는 이메일입니다.")
+        return email
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm):
         model = get_user_model()
-        fields = ('email', 'nickname')
+        fields = ('username', 'email')
--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    nickname = models.CharField(max_length=10)
+    username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
