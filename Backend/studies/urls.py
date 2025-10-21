499ea59ed7e71a5c4c50e282eb210977005699f4
YeonWookJeon <jadam41817@gmail.com>
2025-10-21 14:00:32 +0900

    get_my_study function

--- Backend/studies/urls.py
+++ Backend/studies/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('create_study/', views.create_study, name='create_study'),
     path('join/', views.join, name='join'),
     path('leave/', views.leave, name='leave'),
+    path('get_my_study/', views.get_my_study, name='get_my_study'),
     
 ]
