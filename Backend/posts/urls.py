51d17c38f54ec897ef5f26f42fbc91e9587edc7e
RASIT <shjh0815@naver.com>
2025-11-19 15:28:17 +0900

    Post

--- Backend/posts/urls.py
+++ Backend/posts/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('create_notice/', views.create_notice, name='create_notice'),
-    path('update_notice/', views.update_notice, name='update_notice'),
-    path('read_notice', views.read_notice, name='read_notice'),
-    path('delete_notice', views.delete_notice, name='delete_notice'),
+    path('create_notice/', views.notice_create, name='notice_create'),
+    path('notice_detail/<int:notice_id>', views.notice_detail, name='notice_detail'),
 
 ]
--- Backend/studies/urls.py
+++ Backend/studies/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'studies'
@@ -7,5 +7,8 @@ urlpatterns = [
     path('study/', views.study, name='study'),
     path('join/', views.join, name='join'),
     path('leave/', views.leave, name='leave'),
+    path('<int:study_id>', views.study_detail, name='study_detail'),
     path('study_list/', views.study_list, name='study_list'),
+    path('<int:study_id>/posts/', include('posts.urls')),
+    
 ]
