188d3c13cdeaae1e96af0a288927442338ec8631
Shin Jonghyeok <shjh0815@naver.com>
2025-12-08 13:53:08 +0900

    exception handling

--- Backend/posts/views.py
+++ Backend/posts/views.py
@@ -64,7 +64,10 @@ def notice_create(request, study_id):
                     "author": user.username,
                     "url": f"{settings.VUE_API_URL}studies/{study_id}/notice/{notice.id}"
                 }
-                requests.post(url, json=payload)
+                try:
+                    requests.post(url, json=payload)
+                except:
+                    pass
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
