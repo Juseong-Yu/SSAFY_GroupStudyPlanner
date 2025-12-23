e321d7cadb538e97201606b6358ed2a36e85a40d
RASIT <shjh0815@naver.com>
2025-12-23 11:50:05 +0900

    Edit: change DB

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -156,18 +156,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # "default": {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": "Nestudy_db",
-    #     "USER": "nestudy",
-    #     "PASSWORD": DB_PASSWORD,
-    #     "HOST": "localhost",
-    #     "PORT": "5432",
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Nestudy_db",
+        "USER": "nestudy",
+        "PASSWORD": DB_PASSWORD,
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
 }
 
 
