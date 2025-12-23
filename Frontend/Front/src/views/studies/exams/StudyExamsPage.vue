4bf7aa794101d0014bcbb1bd78bb4e144d99ff91
RASIT <shjh0815@naver.com>
2025-12-23 15:48:03 +0900

    Edit: Typo

--- Frontend/Front/src/views/studies/exams/StudyExamsPage.vue
+++ Frontend/Front/src/views/studies/exams/StudyExamsPage.vue
@@ -171,7 +171,7 @@ import { ensureCsrf, getCookie } from '@/utils/csrf_cors'
 import { useStudyRoleStore } from '@/stores/studyRoleStore'
 import { useUserStore } from '@/stores/user'
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8000'
+const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
 
 const route = useRoute()
 const router = useRouter()
